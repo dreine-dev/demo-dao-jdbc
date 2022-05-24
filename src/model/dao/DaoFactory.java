2808ae725104a4e684668626c04ec1b764b63947
dreine-dev <dreineantonio@hotmail.com>
2022-05-24 17:33:26 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,10 +1,11 @@
 package model.dao;
 
+import db.DB;
 import model.dao.implemente.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createdSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
