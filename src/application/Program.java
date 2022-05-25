4b650353f3c2709cc53574107511897744ed58ae
dreine-dev <dreineantonio@hotmail.com>
2022-05-25 15:57:03 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,8 +30,14 @@ public class Program {
 			for(Seller obj : list) {
 				System.out.println(obj);
 		}
+			
+			System.out.println("\n=== TESTE 4: seller insert ====");
+			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, new Department(2, null));
+			sellerDao.insert(newSeller);
+			System.out.println("Insert new Id" + newSeller.getId());
+		}
 		
 		
 	}
 
-}
+
