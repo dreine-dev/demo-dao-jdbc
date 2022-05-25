5fc7609fdd9826e13f352032c45a5c5255bb6798
dreine-dev <dreineantonio@hotmail.com>
2022-05-25 08:59:34 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,8 +2,11 @@ package application;
 
 
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 
@@ -14,10 +17,18 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createdSellerDao();
 		
-		System.out.println("=== TESTE 1: seller findById ===");
+		System.out.println("=== TESTE 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-	
 		System.out.println(seller);
+		
+		
+		System.out.println("\n=== TESTE 2: seller findByDepartment ====");
+		List<Seller> list = sellerDao.findByDepartment(new Department(2, null));
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		
 	}
 
 }
