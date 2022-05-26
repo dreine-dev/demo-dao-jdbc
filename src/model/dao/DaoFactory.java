617b0e39c69aaffab27afc47df052f85ad2fd8da
dreine-dev <dreineantonio@hotmail.com>
2022-05-26 10:30:15 -0300

    insert department

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.implemente.DepartmentDaoJDBC;
 import model.dao.implemente.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	public static SellerDao createdSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createdDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 }
